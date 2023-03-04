994256fa2c09aa484508edd1af451b9287c000dc
Hiroten31 <hiroten31@gmail.com>
2023-03-04 22:38:18 +0100

    Reorganized class to be more readable.

--- src/main/java/classes/User.java
+++ src/main/java/classes/User.java
@@ -12,14 +12,17 @@ import static java.time.temporal.ChronoUnit.DAYS;
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = -944824420716579638L;
+
     private String name, surname, userID;
     private Integer phoneNumber;
     private final LocalDate joinDate;
-    private List<LocalDate> dateOfRenting = new ArrayList<>();
-    private List<Item> ItemsRented = new ArrayList<>();
+    private final List<LocalDate> dateOfRenting = new ArrayList<>();
+    private final List<Item> ItemsRented = new ArrayList<>();
     static ArrayList<User> Users = new ArrayList<>();
     private static int monthsForFree;
     private static double costPerDayOfDelay;
+
+    //Basic method to create an object
     public User(String name, String surname, Integer phoneNumber){
         this.name = name;
         this.surname = surname;
@@ -28,68 +31,83 @@ public class User implements Serializable {
         userID = phoneNumber.toString();
         Users.add(this);
     }
-    public String getName() {
-        return name;
-    }
+
+    //Getters and Setters
     public void setName(String name){
         this.name = name;
     }
-    public String getSurname() {
-        return surname;
-    }
+
     public void setSurname(String surname) {
         this.surname = surname;
     }
-    public String getFullName(){
-        return name+" "+surname;
-    }
+
     public String getUserID() {
         return userID;
     }
+
     public Integer getPhoneNumber() {
         return phoneNumber;
     }
+
     public void setPhoneNumber(Integer phoneNumber){
         this.phoneNumber = phoneNumber;
         userID = phoneNumber.toString();
     }
+
     public LocalDate getJoinDate() {
         return joinDate;
     }
+
     public List<LocalDate> getDateOfRenting(){
         return dateOfRenting;
     }
-    public double getCost(int i){
-        return DAYS.between(getDateOfRenting().get(i).plusMonths(monthsForFree), LocalDate.now())*costPerDayOfDelay;
-    }
-    public LocalDate getDateOfReturning(int i){
-        return getDateOfRenting().get(i).plusMonths(monthsForFree);
-    }
-    public List<Item> getRentedItems(){
+
+    public List<Item> getItemsRented(){
         return ItemsRented;
     }
+
     public static ArrayList<User> getUserList(){
         return Users;
     }
+
     public static void setUsers(ArrayList<User> users){
         Users = users;
     }
+
     public static int getMonthsForFree(){
         return monthsForFree;
     }
+
     public static void setMonthsForFree(int i){
         monthsForFree = i;
     }
+
     public static double getCostPerDayOfDelay(){
         return costPerDayOfDelay;
     }
+
     public static void setCostPerDayOfDelay(double i){
         costPerDayOfDelay = i;
     }
+
+    //Methods
+    public String getFullName(){
+        return name+" "+surname;
+    }
+
+    public LocalDate getDateOfReturning(int i){
+        return getDateOfRenting().get(i).plusMonths(monthsForFree);
+    }
+
+    public double getCost(int i){
+        return DAYS.between(getDateOfRenting().get(i).plusMonths(monthsForFree), LocalDate.now())*costPerDayOfDelay;
+    }
+
     public void rentsItem(Item item){
         ItemsRented.add(item);
         dateOfRenting.add(LocalDate.of(2022, Month.OCTOBER, ((phoneNumber%10)*item.getId())%28));
     }
+
     public void returnsItem(Item item){
         dateOfRenting.remove(ItemsRented.indexOf(item));
         ItemsRented.remove(item);
