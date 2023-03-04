8d5f86647b09050a1b25bfb0435c511187c9b327
Hiroten31 <hiroten31@gmail.com>
2023-03-04 16:34:38 +0100

    Reorganized class to be more readable.

--- src/main/java/classes/Book.java
+++ src/main/java/classes/Book.java
@@ -1,41 +1,41 @@
 package classes;
 
 public class Book extends Item {
-    private String author, publisher, description;
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-    public String getPublisher() {
-        return publisher;
-    }
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
+    private String author, publisher, description;
+
+    //Basic method to create an object
     public Book(String name, String author, String publisher, String description, int amountAvailable, int amountRented){
         this.name = name;
         this.author = author;
         this.publisher = publisher;
         this.description = description;
-        if(amountAvailable>=0) {
-            this.amountAvailable = amountAvailable;
-        } else {
-            System.out.println("You gave an incorrect number! Amount available will be set to 0.");
-            this.amountAvailable = 0;
-        }
-        if(amountRented>=0){
-            this.amountRented = amountRented;
-        } else {
-            System.out.println("You gave an incorrect number! Amount rented will be set to 0.");
-            this.amountRented = 0;
-        }
+        this.amountAvailable = amountAvailable;
+        this.amountRented = amountRented;
     }
+
+    //Getters and Setters
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //Implementation of the method from Item.class
     @Override
     public void getInfo(){
         System.out.println("Book: " + getName());
