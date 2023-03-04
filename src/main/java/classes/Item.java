4025a7b19de6ea74bcd50731acfda964a444c26b
Hiroten31 <hiroten31@gmail.com>
2023-03-04 22:37:30 +0100

    Reorganized class to be more readable.

--- src/main/java/classes/Item.java
+++ src/main/java/classes/Item.java
@@ -8,67 +8,71 @@ import java.util.List;
 public abstract class Item implements Serializable {
     @Serial
     private static final long serialVersionUID = -8254487033175773570L;
+
     private static int count;
-    protected int id, amountAvailable = 0, amountRented = 0;
+    private final int id;
+    protected int amountAvailable = 0, amountRented = 0;
     protected String name;
-    List<User> rentedByUsersID = new ArrayList<>();
+    private final List<User> rentedByUsersID = new ArrayList<>();
     static ArrayList<Item> Items = new ArrayList<>();
+
+    //Method used to make unique ID for every single item created and adding it to a list.
     Item() {
         ++count;
         this.id = count;
         Items.add(this);
     }
+
+    //Getters and Setters
     public int getId(){
         return id;
     }
+
     public int getAmountAvailable(){
         return amountAvailable;
     }
-    public void setAmountAvailable(int amountAvailable) {
-        this.amountAvailable = amountAvailable;
-    }
+
     public int getAmountRented(){
         return amountRented;
     }
-    public void setAmountRented(int amountRented) {
-        this.amountRented = amountRented;
-    }
+
     public String getName(){
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public static ArrayList<Item> getItemList(){
         return Items;
     }
+
     public static void setItems(ArrayList<Item> items){
         Items = items;
         if(items.size()!=0)
-            count = items.get(items.size()-1).getId();
+            count = items.get(items.size()-1).getId(); //It sets our static count variable to right value, very important due to lack of serialization of static variables.
     }
+
     public List<User> getRentedByUserID(){
         return rentedByUsersID;
     }
+
+    //Methods
     public void rentItem(User user){
-        if(amountAvailable>0) {
-            amountAvailable--;
-            amountRented++;
-            rentedByUsersID.add(user);
-            user.rentsItem(this);
-        } else {
-            System.out.println("You can't rent this item! There is not a single one available!");
-        }
+        amountAvailable--;
+        amountRented++;
+        rentedByUsersID.add(user);
+        user.rentsItem(this);
     }
+
     public void returnItem(User user){
-        if(amountRented>0) {
-            amountAvailable++;
-            amountRented--;
-            rentedByUsersID.remove(user);
-            user.returnsItem(this);
-        } else {
-            System.out.println("You can't return this item! There is not a single one rented!");
-        }
+        amountAvailable++;
+        amountRented--;
+        rentedByUsersID.remove(user);
+        user.returnsItem(this);
     }
+
+    //Simple interface method, which is used to print out all the unique information of an item.
     public abstract void getInfo();
 }
